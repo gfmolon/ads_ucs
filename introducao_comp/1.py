487ff36e93f730ce9b64617d77704f3264c53b29
Guilherme <gfmolon@gmail.com>
2025-04-04 21:56:31 -0300

    Create 1.py

---
+++ introducao_comp/1.py
@@ -0,0 +1,7 @@
+n1 = int(input("Informe um valor do tipo inteiro"))
+n2 = int(input("Informe um outro valor do tipo inteiro"))
+
+m = n1*n2
+
+print(f"Números digitados: {n1} e {n2}")
+print(f"O resultado da multiplicação é {m}")
