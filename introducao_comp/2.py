98faf02eb42a3d6b8b0585d1c793f2294c3674b7
Guilherme <gfmolon@gmail.com>
2025-04-04 22:02:43 -0300

    Create 2.py

---
+++ introducao_comp/2.py
@@ -0,0 +1,16 @@
+n1 = float(input("Digite o primeiro valor"))
+n2 = float(input("Digite o segundo valor"))
+
+print(f"Números digitados: {n1:.2f} e {n2:.2f}")
+
+soma = n1+n2
+print(f"Soma: {soma:.2f}")
+
+sub = n1-n2
+print(f"Subtração: {sub:.2f}")
+
+mult = n1*n2
+print(f"Multiplicação: {mult:.2f}")
+
+div = n1/n2
+print(f"Divisão: {div:.2f}")
