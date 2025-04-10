4c4b33d3d00397c43aca9cbcfd49be58dcdc63b4
Guilherme <gfmolon@gmail.com>
2025-04-10 20:55:37 -0300

    Create motor2.py

---
+++ motor2.py
@@ -0,0 +1,17 @@
+
+
+motor = input("Qual motor deseja acionar? (1 ou 2)\n")
+direcao = input("Qual direção? (F ou T)\n")
+
+
+if motor == "1" or motor == "2" and direcao == "F" or direcao == "T":
+    if motor == "1" and direcao == "F":
+        print("Motor 1 andar pra frente.")
+    elif motor == "2" and direcao == "F":
+        print("Motor 2 andar pra frente.")
+    elif motor == "1" and direcao == "T":
+        print("Motor 1 andar pra trás.")
+    elif motor == "2" and direcao == "T":
+        print("Motor 2 andar pra trás.")
+else:
+        print("Combinação inválida")
