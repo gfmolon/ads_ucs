5b819272baac5411eb6838782648402dfced2b49
Guilherme <gfmolon@gmail.com>
2025-04-10 20:10:15 -0300

    Create triangulo.py

---
+++ triangulo.py
@@ -0,0 +1,12 @@
+#Slides motor
+
+ligar_m = input("Número do motor:\n")
+
+if ligar_m == "1":
+    print("Andar para frente motor 1")
+    print("Desligar motor 1")
+elif ligar_m == "2":
+    print("Andar para frente motor 2")
+    print("Desligar motor 2")
+else:
+    print("Valor inválido. Tente novamente.")
