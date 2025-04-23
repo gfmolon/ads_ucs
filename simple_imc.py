0416001d7f1171615c896a27d52b403ada846b25
Guilherme <gfmolon@gmail.com>
2025-04-23 17:03:54 -0300

    Update simple_imc.py
    Pergunta se quer continuar

--- simple_imc.py
+++ simple_imc.py
@@ -1,15 +1,22 @@
-nome = input("Informe o nome")
-altura = float(input("Informe a altura em metros"))
-peso = float(input("Informe o peso em kg"))
 
-imc = peso / (altura ** 2)
+while True:
+    nome = input("Informe o nome")
+    altura = float(input("Informe a altura em metros"))
+    peso = float(input("Informe o peso em kg"))
 
-if imc <= 18.5:
-    print(f"{nome} tem IMC de {imc:.2f} - Abaixo do peso")
-elif imc <= 24.9:
-    print(f"{nome} tem IMC de {imc:.2f} - Peso normal")
-elif imc <= 29.9:
-    print(f"{nome} tem IMC de {imc:.2f} - Sobrepeso")
-else:
-    print(f"{nome} tem IMC de {imc:.2f} - Obesidade")
+    imc = peso / (altura ** 2)
+
+    if imc <= 18.5:
+        print(f"{nome} tem IMC de {imc:.2f} - Abaixo do peso")
+    elif imc <= 24.9:
+        print(f"{nome} tem IMC de {imc:.2f} - Peso normal")
+    elif imc <= 29.9:
+        print(f"{nome} tem IMC de {imc:.2f} - Sobrepeso")
+    else:
+        print(f"{nome} tem IMC de {imc:.2f} - Obesidade")
+
+    resp = input("Deseja gerar outro? (S/N)").upper()
+    if resp != "S":
+        break
+
 
