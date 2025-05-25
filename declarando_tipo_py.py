ee79aa7a098b3f50fe0f3ceaccefeb762b6eb163
Guilherme <gfmolon@gmail.com>
2025-05-25 01:57:46 -0300

    Create declarando_tipo_py.py

---
+++ declarando_tipo_py.py
@@ -0,0 +1,11 @@
+def nome_usuario(nome: str = "Avestruz") -> str:
+    return nome
+
+
+def nome_pet(nome: str) -> str:
+    return nome
+
+
+print(nome_usuario("Locuraaaaa!!"))
+
+print(nome_pet("Joãozinho."))
