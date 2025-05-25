96778e623c68f4192ed1c2dbd25de19ba7deefe7
Guilherme <gfmolon@gmail.com>
2025-05-25 13:19:05 -0300

    Inclusao dos logs!

--- jogo_advinhacao.py
+++ jogo_advinhacao.py
@@ -1,4 +1,7 @@
 import random
+from datetime import datetime
+
+logs = []
 
 
 def jogada_humano() -> int:
@@ -12,6 +15,33 @@ def jogada_computador() -> int:
     return random.randint(1, 5)
 
 
+def dif_jogada(h: int, c: int) -> str:
+    if h > c:
+        dif = h - c
+    else:
+        dif = c - h
+
+    if dif == 1:
+        print("Bateu na trave!! por apenas 1 de diferença!")
+    elif dif == 2:
+        print("2 de Diferença!! Ta quase..")
+    elif dif == 3:
+        print("3 de Diferença!! Vamos lá.")
+    else:
+        print("Falta de sorte!")
+
+
+def log_add(h: int, c: int, lista: list) -> str:
+    log = f"Humano: {h} | Máquina: {c} | {datetime.now()} "
+    lista.append(log)
+
+
+def log_list(lista: list) -> str:
+    print("Histórico de tentativas:")
+    for log in lista:
+        print(log)
+
+
 while True:
 
     jogada_h = jogada_humano()
@@ -20,8 +50,14 @@ while True:
     jogada_c = jogada_computador()
     print(f"Computador: {jogada_c}")
 
+    log_add(h=jogada_h, c=jogada_c, lista=logs)
+
+    if jogada_h != jogada_c:
+        dif_jogada(h=jogada_h, c=jogada_c)
+
     if jogada_h == jogada_c:
         print("Match!! Jogo Encerrado!")
+        log_list(logs)
         break
     else:
         continue
